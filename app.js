78248073655d1c38eb33e8495ab33e41668228df
rachelsomers <find@A5-004007.localdomain>
2018-11-28 17:17:59 -0700

    starter code

---
+++ app.js
@@ -0,0 +1,10 @@
+//Countdown to december 25
+
+//html element grabber
+//the date for today
+//the date we want (dec25)
+//calculation to know how much longer for that date to come
+//display on the html
+
+//event listener, onchange, the change of the difference
+
